6bc2c2ca9a0f40b7a3863fa0cea5ec96fa03a8ae
Heru Wahyudi <heruwahyd@gmail.com>
2026-07-08 10:23:26 +0700

    update db.js pakai DATABASE_URL

--- db.js
+++ db.js
@@ -7,11 +7,11 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT
+  connectionString: process.env.DATABASE_URL,
+  // ssl diperlukan untuk koneksi ke Railway dari luar
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false
 });
 
 module.exports = pool;
